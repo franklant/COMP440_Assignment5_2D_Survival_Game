e1a5001facd3eca9f9d543e4694df2d55495b2b1
franklant <frankboss349@gmail.com>
2025-10-16 23:11:16 -0400

    add player attack

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
     public Animator myAnimator;
     public SpriteRenderer mySpriteRenderer;     // control the way the sprite faces the correct direction
+    public GameObject leftHitBox;
+    public GameObject rightHitBox;
+    public GameObject upHitBox;
+    public GameObject downHitBox;
     public float movementSpeed;
-    private string dirHorizontal;               // keeps track of the players current horizontal direction
-    private string dirVertical;                 // keeps track of the players current vertical direction
+    private string direction = "down";          // keeps track of the players current direction
+    private bool isAttacking = false;
+    private bool isMoving = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,83 +24,220 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        prototypeMovement();    // subject to change
-        handleAnimations();
+        prototypeMovement();            // subject to change
+        handleMovementAnimations();     // move animation
+        handleFightAnimations();        // fight animation
+        handleAttacks();                // attack collision
     }
 
     void prototypeMovement()
     {
-        // Horizontal Movement
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.Space))
         {
-            dirHorizontal = "left";
-            transform.position += (Vector3.left * movementSpeed) * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            dirHorizontal = "right";
-            transform.position += (Vector3.right * movementSpeed) * Time.deltaTime;
+            isMoving = false;
+            isAttacking = true;
         }
         else
         {
-            dirHorizontal = "";
+            isAttacking = false;
         }
 
-        // Vertical Movement
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        // Horizontal Movement
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && !isAttacking)
         {
-            dirVertical = "up";
+            isMoving = true;
+            direction = "left";
+            transform.position += (Vector3.left * movementSpeed) * Time.deltaTime;
+        }
+        else if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && !isAttacking)
+        {
+            isMoving = true;
+            direction = "right";
+            transform.position += (Vector3.right * movementSpeed) * Time.deltaTime;
+        }
+        else if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && !isAttacking)
+        {
+            isMoving = true;
+            direction = "up";
             transform.position += (Vector3.up * movementSpeed) * Time.deltaTime;
         }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        else if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && !isAttacking)
         {
-            dirVertical = "down";
+            isMoving = true;
+            direction = "down";
             transform.position += (Vector3.down * movementSpeed) * Time.deltaTime;
         }
         else
         {
-            dirVertical = "";
+            isMoving = false;
         }
     }
 
-    void handleAnimations()
+    // if the player is in it's moving state, the proper animations will play depending on the current move direction of the player
+    void handleMovementAnimations()
     {
-        if (dirHorizontal != "")
+        if (isMoving)
         {
-            myAnimator.SetBool("isMovingHorizontal", true);
-            //myAnimator.SetBool("isMovingVertical", false);
+            isAttacking = false;
+            myAnimator.SetBool("isMoving", true);
+            myAnimator.SetBool("isAttacking", false);
 
-            if (dirHorizontal == "left")
+            if (direction == "left" && isMoving)
             {
+                myAnimator.SetBool("isLeftRight", true);
                 mySpriteRenderer.flipX = true;
             }
-            else
+            else if (direction == "right" && isMoving)
             {
+                myAnimator.SetBool("isLeftRight", true);
                 mySpriteRenderer.flipX = false;
             }
-        }
-        else
-        {
-            myAnimator.SetBool("isMovingHorizontal", false);
-        }
-
-        if (dirVertical != "")
-        {
-            myAnimator.SetBool("isMovingVertical", true);
-
-            // check if going backward
-            if (dirVertical == "up")
+            else
             {
-                myAnimator.SetBool("flipY", true);
+                myAnimator.SetBool("isLeftRight", false);
+            }
+
+            if (direction == "up" && isMoving)
+            {
+                myAnimator.SetBool("isUp", true);
             }
             else
             {
-                myAnimator.SetBool("flipY", false);
+                myAnimator.SetBool("isUp", false);
+            }
+
+            if (direction == "down" && isMoving)
+            {
+                myAnimator.SetBool("isDown", true);
+            }
+            else
+            {
+                myAnimator.SetBool("isDown", false);
             }
         }
         else
         {
-            myAnimator.SetBool("isMovingVertical", false);
+            myAnimator.SetBool("isMoving", false);
+        }
+    }
+
+    // if the player is in the attacking state, the proper attack animation will play based on the current direction of the player
+    void handleFightAnimations()
+    {
+        if (isAttacking)
+        {
+            isMoving = false;
+            myAnimator.SetBool("isMoving", false);
+            myAnimator.SetBool("isAttacking", true);
+
+            if (direction == "left")
+            {
+                myAnimator.SetBool("isLeftRight", true);
+                mySpriteRenderer.flipX = true;  // flip the sprite of the play
+
+                leftHitBox.SetActive(true);
+                rightHitBox.SetActive(false);
+            }
+            else if (direction == "right")
+            {
+                myAnimator.SetBool("isLeftRight", true);
+                mySpriteRenderer.flipX = false;
+
+                rightHitBox.SetActive(true);
+                leftHitBox.SetActive(false);
+            }
+            else
+            {
+                myAnimator.SetBool("isLeftRight", false);
+
+                rightHitBox.SetActive(false);
+                leftHitBox.SetActive(false);
+            }
+
+            if (direction == "up")
+            {
+                myAnimator.SetBool("isUp", true);
+                upHitBox.SetActive(true);
+            }
+            else
+            {
+                upHitBox.SetActive(false);
+            }
+
+            if (direction == "down")
+            {
+                myAnimator.SetBool("isDown", true);
+                downHitBox.SetActive(true);
+            }
+            else
+            {
+                downHitBox.SetActive(false);
+            }
+        }
+        else
+        {
+            myAnimator.SetBool("isAttacking", false);
+            upHitBox.SetActive(false);
+            downHitBox.SetActive(false);
+            leftHitBox.SetActive(false);
+            rightHitBox.SetActive(false);
+        }
+    }
+    
+    // when a directional hit box is active, the program will check over every object overlapping with the hit box
+    // from there we can check what object the player has hit and perform the proper operation
+    void handleAttacks()
+    {
+        // check collision from the left hit box
+        if (leftHitBox.activeInHierarchy)
+        {
+            List<Collider2D> results = new List<Collider2D>();
+            leftHitBox.GetComponent<Collider2D>().Overlap(results);
+
+            foreach (Collider2D c in results)
+            {
+                // find a specifc collision object by name
+                Debug.Log(c.name);
+            }
+        }
+
+        // check collision from the right hit box
+        if (rightHitBox.activeInHierarchy)
+        {
+            List<Collider2D> results = new List<Collider2D>();
+            rightHitBox.GetComponent<Collider2D>().Overlap(results);
+
+            foreach (Collider2D c in results)
+            {
+                // find a specifc collision object by name
+                Debug.Log(c.name);
+            }
+        }
+
+        // check collision from the top hit box
+        if (upHitBox.activeInHierarchy)
+        {
+            List<Collider2D> results = new List<Collider2D>();
+            upHitBox.GetComponent<Collider2D>().Overlap(results);
+
+            foreach (Collider2D c in results)
+            {
+                // find a specifc collision object by name
+                Debug.Log(c.name);
+            }
+        }
+        
+        // check collision from the bottom hit box
+        if (downHitBox.activeInHierarchy)
+        {
+            List<Collider2D> results = new List<Collider2D>();
+            downHitBox.GetComponent<Collider2D>().Overlap(results);
+
+            foreach (Collider2D c in results)
+            {
+                // find a specifc collision object by name
+                Debug.Log(c.name);
+            }
         }
     }
 }
